cfc94908e2ad83809fe14ed1564cffff748aee2e
Fuguo Xue <fx2100@nyu.edu>
2023-10-04 16:01:48 -0400

    Update sketch.js

--- data-structures/sketch.js
+++ data-structures/sketch.js
@@ -1,27 +1,35 @@
-let xPos = 0;
-let yPos = 150;
-
-let xVel = 2;
-
-let cDiam = 40;
+let xPos = [];
+let yPos = [];
+let xVel = [];
+let cDiam = [];
+let numCircles = 10;
 
 function setup() {
   createCanvas(800, 600);
+  // Instead of declaring an array for eahc of the xPos, yPos, xVel, and cDiam, let for loop do it for us in setup()
+  for (let i=0; i<numCircles; i++) {
+    xPos.push(0);
+    yPos.push(random(30, height-30));
+    xVel.push(random(2,5));
+    cDiam.push(random(20,70));
+  }
 }
 
 function draw() {
   background("lightblue");
   fill(220, 10, 100);
 
-  // draw circle
-  ellipse(xPos, yPos, cDiam, cDiam);
+  for (let i=0; i<numCircles; i++) {
+    // draw circle
+    ellipse(xPos[i], yPos[i], cDiam[i], cDiam[i]);
 
-  // update pos
-  xPos = xPos + xVel;
+    // update pos
+    xPos[i] = xPos[i] + xVel[i];
 
-  // if reset is needed: pick random diameter and reset x
-  if (xPos > width + cDiam) {
-    cDiam = random(20, 75);
-    xPos = 0 - cDiam;
+    // if reset is needed: pick random diameter and reset x
+    if (xPos[i] > width + cDiam[i]) {
+      cDiam[i] = random(20, 75);
+      xPos[i] = 0 - cDiam[i];
+    }
   }
 }
